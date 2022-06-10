cb1c371b9cb9126f62a682d6f3f8e1f502548481
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2022-06-10 11:02:39 +0800

    move the close event listener to close element

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const createNotif = (
     title = "Sample Title",
     content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, consequatur officiis iure pariatur maxime natus beatae officia distinctio doloribus modi?",
     autoClose = true,
+    time = 3000
 ) => {
     const identifier =
         Math.random().toString(36).substring(2, 15) +
@@ -18,23 +19,38 @@ const createNotif = (
     document
         .querySelector(".notif-container")
         .insertAdjacentHTML("beforeend", notification);
-    document.getElementById(`${identifier}`).addEventListener("click", (e) => {
-        const el = e.currentTarget;
-        el.classList.add("remove-notif");
-        setTimeout(() => { el.remove() }, 1000)
-    })
+    const notifEl = document.getElementById(`${identifier}`)
+    const notifClose = notifEl.querySelector(".notif-close")
+    notifClose.addEventListener("click", closeNotif)
     if (autoClose) {
         setTimeout(() => {
             const el = document.getElementById(`${identifier}`);
             if (!el) return;
             el.classList.add("remove-notif");
             setTimeout(() => { el.remove() }, 1000)
-        }, 3000);
+        }, time);
     }
 };
 
-createNotif();
-createNotif();
-createNotif();
-createNotif();
-createNotif();
+const closeNotif = e => {
+    const el = e.currentTarget.parentElement.parentElement;
+    el.classList.add("remove-notif");
+    setTimeout(() => { el.remove() }, 1000)
+
+    const allNotif = document.querySelectorAll(".notif");
+    allNotif.forEach(notif => {
+        if (notif === el) return;
+        notif.classList.add("slide-down");
+    })
+}
+
+const paragraphs = [
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, consequatur officiis iure pariatur maxime natus beatae officia distinctio doloribus modi?",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, consequatur officiis iure pariatur maxime natus beatae officia distinctio doloribus modi?",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, consequatur officiis iure pariatur maxime natus beatae officia distinctio doloribus modi?"
+]
+createNotif("Title", paragraphs[0], false);
+createNotif("Title", paragraphs[0], false);
+createNotif("Title", paragraphs[0], false);
+createNotif("Title", paragraphs[0], false);
+createNotif("Title", paragraphs[0], false);
